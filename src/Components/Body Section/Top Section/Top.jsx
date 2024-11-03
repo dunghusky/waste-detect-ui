bcfc53e00454f7c7e08ef1abb59dd1ff1542451b
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-11-03 20:36:59 +0700

    add listing

--- src/Components/Body Section/Top Section/Top.jsx
+++ src/Components/Body Section/Top Section/Top.jsx
@@ -1,9 +1,94 @@
 // import React from 'react'
 import './top.css'
 
+import { BiSearchAlt } from "react-icons/bi";
+import { TbMessageDots } from "react-icons/tb";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { BsArrowRightShort } from "react-icons/bs";
+// import { BsQuestionCircle } from "react-icons/bs";
+
+import img from "../../../Assets/user1.jpg"
+import img2 from "../../../Assets/cay_den.png"
+import video from "../../../LoginAssets/video.mp4"
+
 function Top() {
   return (
-    <div>Top</div>
+    <div className='topSection'>
+      <div className="headerSection flex">
+        <div className="title">
+          <h1>Welcome to Pranti.</h1>
+          <p>Hello IsraTech, Welcome back!</p>
+        </div>
+
+        <div className="searchBar flex">
+          <input type="text" placeholder='Search Dashboard' />
+          <BiSearchAlt className='icon'/>
+        </div>
+
+        <div className="adminDiv flex">
+          <TbMessageDots className='icon'/>
+          <IoNotificationsOutline className='icon'/>
+          <div className="adminImage">
+            <img src={img} alt="Admin Image" />
+          </div>
+        </div>
+      </div>
+
+      <div className="cardSection flex">
+
+        <div className="rightCard flex">
+          <h1>Create and sell extraordinary products</h1>
+          <p>The world`s fast growing industy today are natural made products</p>
+
+          <div className="buttons flex">
+            <button className="btn">Explore More</button>
+            <button className="btn transparent">Top Sellers</button>
+          </div>
+
+          <div className="videoDiv">
+            <video src={video} autoPlay loop muted></video>
+          </div>
+        </div>
+
+        <div className="leftCard flex">
+          <div className="main flex">
+            <div className="textDiv">
+              <h1>My Stat</h1>
+
+              <div className="flex">
+                <span>
+                  Today <br /> <small>4 Orders</small>
+                </span>
+                <span>
+                  This Month <br /> <small>175 Orders</small>
+                </span>
+              </div>
+
+              <span className="flex link">
+                Go to my orders <BsArrowRightShort className='icon'/>
+              </span>
+            </div>
+
+            <div className="imgDiv">
+              <img src={img2} alt="Image Name" />
+            </div>
+
+            {/* <div className="sideBarCard">
+              <BsQuestionCircle className='icon'/>
+              <div className="cardContent">
+                <div className="circle1"></div>
+                <div className="circle2"></div>
+
+                <h3>Help Center</h3>
+                <p>Having trouble in Planti, please contact us from for more questions.</p>
+                
+                <button className='btn'>Go to help center</button>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
