27b821237433ac99112fd6eb4193d0f83973df84
DangThiThuyDung06 <thuydungaelita@gmail.com>
2024-11-08 21:58:50 +0700

    complete activity

--- src/Components/Body Section/Activity Section/Activity.jsx
+++ src/Components/Body Section/Activity Section/Activity.jsx
@@ -1,9 +1,73 @@
 // import React from 'react'
 import './activity.css'
+import { BsArrowRightShort } from "react-icons/bs";
 
-function Activity() {
+import img from "../../../Assets/user2.jpg";
+
+const Activity = () => {
   return (
-    <div>Activity</div>
+    <div className='activitySection'>
+      <div className="heading flex">
+        <h1>Resent Activity</h1>
+        <button className="btn flex">
+          See All
+          <BsArrowRightShort className='icon'/>
+        </button>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={img} alt="Customers Image" />
+          <div className="customerDetails">
+            <span className='name'>Thuy Dung</span>
+            <small>Oder a new plants</small>
+          </div>
+          <div className="duration">
+            2 minutes ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={img} alt="Customers Image" />
+          <div className="customerDetails">
+            <span className='name'>Thuy Dung</span>
+            <small>Oder a new plants</small>
+          </div>
+          <div className="duration">
+            2 minutes ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={img} alt="Customers Image" />
+          <div className="customerDetails">
+            <span className='name'>Thuy Dung</span>
+            <small>Oder a new plants</small>
+          </div>
+          <div className="duration">
+            2 minutes ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={img} alt="Customers Image" />
+          <div className="customerDetails">
+            <span className='name'>Thuy Dung</span>
+            <small>Oder a new plants</small>
+          </div>
+          <div className="duration">
+            2 minutes ago
+          </div>
+        </div>
+        <div className="singleCustomer flex">
+          <img src={img} alt="Customers Image" />
+          <div className="customerDetails">
+            <span className='name'>Thuy Dung</span>
+            <small>Oder a new plants</small>
+          </div>
+          <div className="duration">
+            2 minutes ago
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
