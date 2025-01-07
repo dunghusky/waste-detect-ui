ccf7a8cf658fb876923174261dd394e458359f8b
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-01-07 10:58:46 +0700

    test host

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://52.88.216.148:8080', //http://54.160.228.250:6060',// 'http://127.0.0.1:8000',//'http://52.88.216.148:8000',  // địa chỉ và cổng backend
+      "/api": {
+        target: "https://waste-detect.mekongai.com", //http://54.160.228.250:6060',// 'http://127.0.0.1:8000',//'http://52.88.216.148:8000',  // địa chỉ và cổng backend
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, '')  // loại bỏ "/api" prefix trong request
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
