c9891238dbd7d0ddb4c13c43b50ce0c882415181
DangThiThuyDung06 <thuydungaelita@gmail.com>
2025-01-10 00:33:29 +0700

    write logic image

--- src/Components/Dashboard/Components/Image/ImageStream/ImageStream.jsx
+++ src/Components/Dashboard/Components/Image/ImageStream/ImageStream.jsx
@@ -1,7 +1,12 @@
-import { Select, Slider } from "antd";
+import { message, Slider } from "antd";
+import { useState, useEffect } from "react";
 import TextArea from "antd/es/input/TextArea";
-import { useState } from "react";
 import { MdFileUpload } from "react-icons/md";
+import { Autocomplete, TextField } from "@mui/material";
+import axios from "axios";
+import { fetchWasteRows } from "../../DataManagement/Model/TableModel/DataTWSource";
+import { MdDelete } from "react-icons/md";
+
 // const JsonViewer = ({ jsonString }) => {
 //   // Chuyển chuỗi JSON thành đối tượng JSON
 //   const jsonData = JSON.parse(jsonString);
@@ -26,37 +31,95 @@ import { MdFileUpload } from "react-icons/md";
 const ImageStream = () => {
   const [inputValueCoonfidence, setInputValueCoonfidence] = useState(50);
   const [inputValueOverlap, setInputValueOverlap] = useState(50);
-  // const [inputValueJson, setInputValueJson] = useState("abc");
-  // const [imageOutput, setImageOutput] = useState(
-  //   "../../../../../../public/img/anh.jpg"
-  // );
+  const [inputValueJson, setInputValueJson] = useState("abc");
+  const [imageOutput, setImageOutput] = useState(null);
   const [imageInput, setImageInput] = useState();
   const [isLoadingUploadImage, setIsLoadingUploadImage] = useState(false);
+  const [models, setModels] = useState([]); // Trạng thái lưu danh sách mô hình
+  const [selectedModel, setSelectedModel] = useState(null); // Trạng thái lưu mô hình được chọn
+
+  useEffect(() => {
+    // Lấy dữ liệu mô hình từ API
+    const loadModels = async () => {
+      try {
+        const data = await fetchWasteRows();
+        const formattedData = data.map((item) => ({
+          value: item.id_model,
+          label: item.model_name,
+          link: item.link,
+        }));
+        setModels(formattedData);
+      } catch (error) {
+        console.error("Error fetching models:", error);
+      }
+    };
+
+    loadModels();
+  }, []);
 
   const handleUploadImage = (e) => {
+    if (!selectedModel) {
+      message.error("Vui lòng chọn mô hình!");
+      return;
+    }
+    console.log("Vào upload máy", e);
+
     if (e.target.files && e.target.files.length > 0) {
       setImageInput(e.target.files[0]);
-      //gọi api ở đây
-      handleGetAPIUploadImage(e.target.files[0]);
+      setIsLoadingUploadImage(true);
+      handleGetAPIUploadImage(e.target.files[0], selectedModel);
     }
   };
 
-  const handleGetAPIUploadImage = (fileUpload) => {
-    setIsLoadingUploadImage(true);
+  // const handleGetAPIUploadImage = (fileUpload) => {
+  //   if (!selectedModel || !imageInput) {
+  //     alert("Vui lòng chọn mô hình và tải ảnh!");
+  //     return;
+  //   }
+
+  //   setIsLoadingUploadImage(true);
+  //   try {
+  //     setImageInput("");
+  //     //nếu trả về thì set loading thành false
+  //   } catch (e) {
+  //     setIsLoadingUploadImage(false);
+  //     console.log(e);
+  //   }
+  // };
+
+  const handleGetAPIUploadImage = async (fileUpload, selectedModel) => {
+    if (!fileUpload) {
+      message.error("Vui lòng chọn mô hình và tải ảnh!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("img", fileUpload); // Ảnh đầu vào
+    formData.append("conf", inputValueCoonfidence / 100); // Giá trị Confidence (0-1)
+    formData.append("iou", inputValueOverlap / 100); // Giá trị IOU (0-1)
+    formData.append("path_model", selectedModel.link); // Đường dẫn model
+
     try {
-      setImageInput("");
-      //nếu trả về thì set loading thành false
-    } catch (e) {
+      const response = await axios.post(
+        "/api/v1/image/image_detect",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log("Data res:", response);
+      if (response.status === 200) {
+        setImageOutput(response.data.data);
+      }
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      alert("Xảy ra lỗi khi xử lý ảnh!");
+    } finally {
       setIsLoadingUploadImage(false);
-      console.log(e);
     }
   };
-
-  const optionSelect = [
-    { value: "a", label: "a" },
-    { value: "a", label: "a" },
-    { value: "a", label: "a" },
-  ];
 
   const onChangeConfidence = (newValue) => {
     setInputValueCoonfidence(+newValue);
@@ -65,13 +128,41 @@ const ImageStream = () => {
     setInputValueOverlap(+newValue);
   };
 
+  const handleDeleteImage = () => {
+    setImageInput("");
+    setImageOutput("");
+    setInputValueCoonfidence(50);
+    setInputValueOverlap(50);
+  };
+
+  const handleMouseUp = () => {
+    console.log("On mouse up");
+    handleGetAPIUploadImage(imageInput, selectedModel);
+  };
+
+  const handleSetSelect = (newValue) => {
+    if (newValue) {
+      setSelectedModel(newValue);
+      handleGetAPIUploadImage(imageInput, newValue);
+    }
+  };
+
   return (
     <div className="w-[100%] h-[95%] px-6 py-4">
       <div className="w-full rounded-md border border-slate-200 py-2 px-10 bg-white">
-        <Select options={optionSelect} value={"a"} className="w-full" />
+        <Autocomplete
+          className="w-full"
+          options={models}
+          getOptionLabel={(option) => option.label || ""}
+          value={selectedModel}
+          onChange={(event, newValue) => handleSetSelect(newValue)}
+          renderInput={(params) => (
+            <TextField {...params} variant="outlined" /> //label="Age"
+          )}
+        />
       </div>
       <div className="flex gap-3 justify-center items-center w-full mt-5 h-full">
-        <div className="w-[25%] p-4 rounded-lg border border-slate-200 shadow-sm bg-white h-full">
+        <div className="w-[25%] p-4 rounded-lg border border-slate-200 shadow-sm bg-white h-full flex flex-col items-center gap-2">
           <p className="font-medium mb-2">Upload Image or Video File</p>
           <div className="border border-dashed bg-white border-slate-200 rounded-md p-8 flex flex-col items-center justify-center">
             <p className="font-normal text-xs mb-2">Drop file here or</p>
@@ -92,11 +183,17 @@ const ImageStream = () => {
             </div>
           </div>
           {imageInput && (
-            <img
-              src={URL.createObjectURL(imageInput)}
-              className="rounded-md h-[100px] w-fit"
-              alt="input-image"
-            />
+            <div className="group relative w-fit h-fit mt-4">
+              <img
+                src={URL.createObjectURL(imageInput)}
+                className="rounded-md h-[100px] w-fit"
+                alt="input-image"
+              />
+              <MdDelete
+                className="group-hover:block hidden text-red-500 cursor-pointer absolute right-2 top-2"
+                onClick={handleDeleteImage}
+              />
+            </div>
           )}
         </div>
         <div className="w-[50%] p-5 rounded-lg border border-slate-200 shadow-sm bg-white h-full flex items-center justify-center">
@@ -105,14 +202,14 @@ const ImageStream = () => {
             : imageOutput && (
                 <img
                   src={imageOutput}
-                  className="h-full w-fit rounded-md"
+                  className="h-fit w-fit max-h-full rounded-md"
                   alt="output-image"
                 />
               )}
         </div>
         <div className="w-[25%] p-4 rounded-lg border border-slate-200 shadow-sm bg-white h-full flex flex-col gap-4">
           <div className="border border-slate-200 rounded-lg shadow-sm p-4 w-full">
-            <label>Coonfidence Threshold: {inputValueCoonfidence}%</label>
+            <label>Confidence Threshold: {inputValueCoonfidence}%</label>
             <div className="flex items-center gap-2 text-xs mb-2">
               <lebel>0%</lebel>
               <Slider
@@ -121,6 +218,7 @@ const ImageStream = () => {
                 className="w-full"
                 onChange={onChangeConfidence}
                 value={inputValueCoonfidence}
+                onChangeComplete={handleMouseUp}
               />
               <lebel>100%</lebel>
             </div>
@@ -133,6 +231,7 @@ const ImageStream = () => {
                 className="w-full"
                 onChange={onChangeOverlap}
                 value={inputValueOverlap}
+                onChangeComplete={handleMouseUp}
               />
               <lebel>100%</lebel>
             </div>
--- src/shared/modals/ModalModelCategory.jsx
+++ src/shared/modals/ModalModelCategory.jsx
@@ -1,56 +1,192 @@
-import { Input, Modal } from "antd"
+import { Input, message, Modal } from "antd";
 import PropTypes from "prop-types";
+import { MdDelete } from "react-icons/md";
 
-export const ModalModelCategory = ({isOpen, handleCloseModal, handleOkModal, data, isEditMode, setData, isDataEdit}) => {
-    return(
-        <>
-        <Modal 
-            title={isEditMode ? "Update Model Category" :"Add New Model Category"}
-            open={isOpen} 
-            onCancel={handleCloseModal} 
-            onOk={handleOkModal}
-            width={1000}
-            height={1000}
-            okButtonProps={{disabled:isDataEdit}}
-            okText={isEditMode ? "Update Model" : "Add New Model"}
-        >
-            <div className="grid grid-cols-12">
-               <div className="col-span-4">
-                 <label>Tên mô hình</label>
-                <Input 
-                    value={data && data.model_name && data.model_name}
-                    className="w-full"
-                    onChange={(e) => setData((prev) => ({ ...prev, model_name: e.target.value }))}/>
-               </div>
+export const ModalModelCategory = ({
+  isOpen,
+  handleCloseModal,
+  handleOkModal,
+  data,
+  isEditMode,
+  setData,
+  isDataEdit,
+}) => {
+  const allowdFileTypes = {
+    "image/png": true,
+    "image/jpeg": true,
+    "image/jpg": true,
+    "text/csv": true, // Thêm MIME type cho file CSV
+    "application/vnd.ms-excel": true,
+    "text/plain": true
+  };
+  const onChangeUploadFile = (file, fileType) => {
+    if (file) {
+      const isInvalidFileType = !allowdFileTypes[file.type];
+      if (!isInvalidFileType) {
+        setData((prev) => {
+          console.log(prev);
+          const dataPrev = prev ? prev : {};
+          return { ...dataPrev, [fileType]: file };
+        });
+      } else {
+        message.error("Please upload file in allowed formats!");
+        return;
+      }
+    }
+  };
 
-               <div className="col-span-4">
-                 <label>Link</label>
-                <Input 
-                    value={data && data.link && data.link} 
-                    className="w-full"
-                    onChange={(e) => setData((prev) => ({ ...prev, link: e.target.value }))}
-                />
-               </div>
+  const handleDeleteFile = (fileType) => {
+    setData((prev) => {
+      return { ...prev, [fileType]: null };
+    });
+  };
 
-               <div className="col-span-4">
-                 <label>Ghi chú</label>
-                <Input 
-                    value={data && data.note && data.note} 
-                    className="w-full"
-                    onChange={(e) => setData((prev) => ({ ...prev, note: e.target.value }))}/>
-               </div>
+  const isURL = (value) => {
+    try {
+      new URL(value);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  const imgSrc = () => {
+    if (data && data.img) {
+      return isURL(data.img) ? data.img : URL.createObjectURL(data.img);
+    }
+  };
+  return (
+    <>
+      <Modal
+        title={isEditMode ? "Update Model Category" : "Add New Model Category"}
+        open={isOpen}
+        onCancel={handleCloseModal}
+        onOk={handleOkModal}
+        width={1000}
+        height={1000}
+        okButtonProps={{ disabled: isDataEdit }}
+        okText={isEditMode ? "Update Model" : "Add New Model"}
+      >
+        <div className="grid grid-cols-12">
+          <div className="col-span-4">
+            <label>Tên mô hình</label>
+            <Input
+              value={data && data.model_name && data.model_name}
+              className="w-full"
+              onChange={(e) =>
+                setData((prev) => ({ ...prev, model_name: e.target.value }))
+              }
+            />
+          </div>
+
+          <div className="col-span-4">
+            <label>Link</label>
+            <Input
+              value={data && data.link && data.link}
+              className="w-full"
+              onChange={(e) =>
+                setData((prev) => ({ ...prev, link: e.target.value }))
+              }
+            />
+          </div>
+          
+          <div className="col-span-4">
+            <label>Ghi chú</label>
+            <Input
+              value={data && data.note && data.note}
+              className="w-full"
+              onChange={(e) =>
+                setData((prev) => ({ ...prev, note: e.target.value }))
+              }
+            />
+          </div>
+
+          {/* Date Picker */}
+          <div className="col-span-4 mt-4">
+            <label>Ngày</label>
+            <Input
+              type="date"
+              className="w-full"
+              onChange={(e) =>
+                setData((prev) => ({ ...prev, date: e.target.value }))
+              }
+              value={data && data.date}
+            />
+          </div>
+
+          <div className="col-span-12 flex gap-4">
+            {/* Upload Image */}
+            <div className="flex flex-col !justify-start !items-start">
+              <p>Upload Image</p>
+              <input
+                type="file"
+                id="upload-img"
+                hidden
+                onChange={(e) => onChangeUploadFile(e.target.files[0], "img")}
+              />
+              <div className="flex gap-2">
+                {data && data.img && (
+                  <div className="relative group">
+                    <MdDelete
+                      onClick={() => handleDeleteFile("img")}
+                      className="absolute right-1 top-1 group-hover:block hidden text-red-500 cursor-pointer"
+                    />
+                    <img
+                      src={imgSrc()}
+                      alt="File image waste"
+                      className="h-[120px] w-fit rounded-lg"
+                    />
+                  </div>
+                )}
+                <label
+                  htmlFor="upload-img"
+                  className="h-[120px] w-[120px] rounded-lg border border-dashed cursor-pointer flex items-center justify-center"
+                >
+                  Select Image
+                </label>
+              </div>
             </div>
-        </Modal>
-        </>
-    )
-}
+
+            {/* Upload CSV */}
+            <div className="flex flex-col !justify-start !items-start">
+              <p>Upload CSV</p>
+              <input
+                type="file"
+                id="upload-csv"
+                hidden
+                onChange={(e) => onChangeUploadFile(e.target.files[0], "csv")}
+              />
+              <div className="flex gap-2">
+                {data && data.csv && (
+                  <div className="relative group">
+                    <MdDelete
+                      onClick={() => handleDeleteFile("csv")}
+                      className="absolute right-1 top-1 group-hover:block hidden text-red-500 cursor-pointer"
+                    />
+                    <p className="text-gray-700">{data.csv.name}</p>
+                  </div>
+                )}
+                <label
+                  htmlFor="upload-csv"
+                  className="h-[120px] w-[120px] rounded-lg border border-dashed cursor-pointer flex items-center justify-center"
+                >
+                  Select CSV
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
 
 ModalModelCategory.propTypes = {
-    isOpen: PropTypes.bool.isRequired,
-    handleCloseModal: PropTypes.func.isRequired,
-    handleOkModal: PropTypes.func.isRequired,
-    data: PropTypes.object.isRequired,
-    isEditMode: PropTypes.bool.isRequired,
-    setData: PropTypes.func.isRequired,
-    isDataEdit: PropTypes.bool.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  handleCloseModal: PropTypes.func.isRequired,
+  handleOkModal: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+  isEditMode: PropTypes.bool.isRequired,
+  setData: PropTypes.func.isRequired,
+  isDataEdit: PropTypes.bool.isRequired,
 };
